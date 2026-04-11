6c1d7a797f4f61901e4296c347c1a65f7e5b97ab
keihanmatcha <161185005+keihanmatcha@users.noreply.github.com>
2026-04-11 21:41:06 +0900

    Update App.tsx

--- mirror-palette/src/app/App.tsx
+++ mirror-palette/src/app/App.tsx
@@ -1,1 +1,19 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { AppRoutes } from './routes';
+import { Layout } from './components/Layout';
+import { ErrorBoundary } from './components/ErrorBoundary';
 
+const App = () => {
+  return (
+    <ErrorBoundary>
+      <BrowserRouter>
+        <Layout>
+          <AppRoutes />
+        </Layout>
+      </BrowserRouter>
+    </ErrorBoundary>
+  );
+};
+
+export default App;
