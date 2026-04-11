217ca378ccd2978011984efd9c988c55a3739d02
keihanmatcha <161185005+keihanmatcha@users.noreply.github.com>
2026-04-11 21:42:27 +0900

    Update storage.ts

--- mirror-palette/src/app/utils/storage.ts
+++ mirror-palette/src/app/utils/storage.ts
@@ -1,1 +1,21 @@
+import { ClothingItem } from '../types';
 
+const STORAGE_KEY = 'mirror_palette_closet';
+
+export const storage = {
+  saveItem: (item: ClothingItem) => {
+    const items = storage.getAllItems();
+    const updated = [...items, item];
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(updated));
+  },
+  
+  getAllItems: (): ClothingItem[] => {
+    const data = localStorage.getItem(STORAGE_KEY);
+    return data ? JSON.parse(data) : [];
+  },
+  
+  removeItem: (id: string) => {
+    const items = storage.getAllItems();
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(items.filter(i => i.id !== id)));
+  }
+};
